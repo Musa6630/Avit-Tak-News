0be4f29604c13f59d6a9bea5bbc8acbe19aba568
musabbir <musabbirrockzz123@gmail.com>
2023-01-15 02:46:21 +0530

    main

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import React, { Component } from 'react'
 // import PropTypes from 'prop-types'
 import Navbar from './components/Navbar'
 import News from './components/News'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-export class App extends Component {
-  static propTypes = {
-
-  }
+export default class App extends Component {
+  pageSize= 6;
 
   render() {
     return (
       <div>
+        <Router>
         <Navbar></Navbar>
-        <News></News>
+        <Routes>
+          
+          <Route exact path='/' element={<News key="general" pageSize={this.pageSize} country="in" category="general"></News>}></Route>
+          <Route exact path='/business' element={<News key="business" pageSize={this.pageSize} country="in" category="business"></News>}></Route>          
+          <Route exact path='/entertainment' element={<News key="entertainment" pageSize={6} country="in" category="entertainment"></News>}></Route>
+          <Route exact path='/general' element={<News key="general" pageSize={this.pageSize} country="in" category="general"></News>}></Route>
+          <Route exact path='/health' element={<News key="health" pageSize={this.pageSize} country="in" category="health"></News>}></Route>
+          <Route exact path='/science' element={<News key="science" pageSize={this.pageSize} country="in" category="science"></News>}></Route>
+          <Route exact path='/sports' element={<News key="sports" pageSize={this.pageSize} country="in" category="sports"></News>}></Route>
+          <Route exact path='/technology' element={<News key="technology" pageSize={this.pageSize} country="in" category="technology"></News>}></Route>
+
+        </Routes>
+        </Router>
       </div>
     )
   }
 }
 
-export default App
+//export default App
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,20 +2,43 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imageUrl, newsUrl, source, author, date } =
+      this.props;
     return (
-      <div>
-        <div className="card" style={{ width: "18rem" }}>
+      <div className="my-3">
+        <div className="card">
+          <span
+            class="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
+            style={{ left: "90%", zIndex: "1" }}
+          >
+            {" "}
+            {source}
+          </span>
           <img
-            src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
+            src={
+              !imageUrl
+                ? "https://www.sammobile.com/wp-content/uploads/2023/01/Apple-Watch-Ultra-720x405.jpg"
+                : imageUrl
+            }
             className="card-img-top"
             alt="..."
           />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
+            <p className="card-text">
+              <small className="text-danger">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read More
             </a>
           </div>
         </div>
